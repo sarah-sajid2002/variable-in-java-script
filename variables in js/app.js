c9ac5ac0d30cbb67b24966ad95d4375ef536a8a6
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-04-05 20:46:09 +0500

    Add files via upload

---
+++ variables in js/app.js
@@ -0,0 +1,28 @@
+// let data_1  = "sarah";
+// let data_2 = "sajid";
+// alert(data_1 + " " + data_2);
+// console.log(data_1 + " " + data_2);
+
+// concatenation
+
+// data_1 = "20";
+// data_2 = 30;
+// console.log(data_1 + data_2);
+// console.log(typeof (data_1 + data_2))
+
+// let s = data_1.includes("h")
+// console.log(s)
+
+// math operators:
+//user input:
+// let userNo = prompt("your fvt number")
+// console.log(userNo)
+
+// let userName = prompt("your name?");
+// alert("welcome " + " " + userName);
+
+// let mathRandom = Math.random();
+// mathRandom = mathRandom * 100;
+// mathRandom = Math.round(mathRandom);
+// console.log(mathRandom)
+alert("sarah sajid")
